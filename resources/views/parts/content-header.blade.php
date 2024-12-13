a8d7c7f96b1b9319ee3308e6ab0d303f6da8cfca
Sandro Rodrigues <contato@sandrorodrigues.net>
2024-12-13 07:53:09 -0300

    Dev

---
+++ resources/views/parts/content-header.blade.php
@@ -0,0 +1,8 @@
+<div class="app-content-header"> <!--begin::Container-->
+    <div class="container-fluid"> <!--begin::Row-->
+
+        @yield('content')
+
+
+    </div> <!--end::Container-->
+</div>
